53132b38fbc382a27dde5438ecc9f5c6b7615e4f
Steven Klenklen <klenksm@dds.dillards.net>
2019-02-05 14:14:07 -0600

    react-router-dom and components

---
+++ react-router-basics/src/components/Error.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Error = () => {
+    return(
+        <div>
+            <h1>Error this page does not exist</h1>
+        </div>
+    )
+}
+
+export default Error;
